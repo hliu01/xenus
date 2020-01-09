9a40124925b5b35690085ec64195c47201aad705
Henry Liu <hliu01@stuy.edu>
2020-01-08 23:55:31 -0500

    fixed bugs

--- app.py
+++ app.py
@@ -8,22 +8,46 @@ import sqlite3, random
 import os
 from flask import flash
 import urllib.request, json
-import utl.dbfunctions
 from os import urandom
 app = Flask(__name__)
 app.secret_key = urandom(32)
 
-utl.dbfunctions.setup()
+#-----------------------------------------------------------------
+
+#DATABASE SETUP
 DB_FILE = "Info.db"
+db = sqlite3.connect(DB_FILE)
+c = db.cursor()
+#Creates USER
+c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='USER' ''')
+if c.fetchone()[0] < 1:
+    c.execute("CREATE TABLE USER(username TEXT, password TEXT);")
+    # TESTS
+    c.execute("INSERT INTO USER VALUES ('{}', '{}')".format("hliu00","hi"))
+    c.execute("INSERT INTO USER VALUES ('{}', '{}')".format("hliu01","hi"))
+
+#Creates Points
+c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='POINTS' ''')
+if c.fetchone()[0] < 1:
+    c.execute("CREATE TABLE POINTS(username TEXT, points INTEGER);")
+    # TESTS
+    c.execute("INSERT INTO POINTS VALUES ('{}', '{}')".format("hliu00",2))
+    c.execute("INSERT INTO POINTS VALUES ('{}', '{}')".format("hliu00",1))
+
+#Creates REVIEWS
+c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='STORIES' ''')
+if c.fetchone()[0] < 1:
+    c.execute("CREATE TABLE STORIES(storyid INTEGER, title TEXT, text BLOB);")
+    # TESTS
+    c.execute("INSERT INTO STORIES VALUES ('{}', '{}', '{}')".format(0, "DD", "0dswdwdw"))
+    c.execute("INSERT INTO STORIES VALUES ('{}', '{}', '{}')".format(1, "DD", "1dswdwdw"))
+    db.commit()
+    db.commit()
+    db.close()
+#-----------------------------------------------------------------
 
 # DICTIONARY FOR IMPORTANT SEARCH DATA
 searchdict = {}
-
-def checkAuth(): #checks if the user is logged in
-    if "userID" in session:
-        return True
-    else:
-        return False
 
 @app.route("/")
 def root():
@@ -54,39 +78,37 @@ def level3():
 
 @app.route("/login")
 def login():
-    # if already logged in, don't display login page
-    if checkAuth():
-        return redirect(url_for('play'))
-    else:
-        return render_template('login.html')
-
-
-@app.route("/auth", methods=["POST"])
-def auth():
-    username = request.form['username']
-    password = request.form['password']
-    db = sqlite3.connect(DB_FILE)
-    c = db.cursor()
-    c.execute("SELECT username,password FROM users WHERE username = ?", (username, ))
-    a = c.fetchone()
-    db.commit()
-    c.close()
-    if a == None: #checks login username and password
-        flash("No user found with given username") #no given username in database
-        return redirect(url_for('login'))
-    elif password != a[1]:
-        flash("Incorrect password") #password is incorrect for given username
-        return redirect(url_for('login'))
-    else: #successfully pass the tests
-        session['username'] = username
-        flash("Welcome " + username + ". You have been logged in successfully.")
+    # if user already logged in, redirects back to discover
+    if 'user' in session:
         return redirect(url_for('root'))
+    # checking to see if things were submitted
+    if (request.args):
+        if (bool(request.args["username"]) and bool(request.args["password"])):
+            # setting request.args to variables to make life easier
+            inpUser = request.args["username"]
+            inpPass = request.args["password"]
+            with sqlite3.connect(DB_FILE) as connection:
+                cur = connection.cursor()
+                q = 'SELECT username, password FROM USER;'
+                foo = cur.execute(q)
+                userList = foo.fetchall()
+                for row in userList:
+                    if inpUser == row[0] and inpPass == row[1]:
+                        session['user'] = inpUser
+                        return(redirect(url_for("root")))
+                flash('Username not found or login credentials incorrect.')
+                return(redirect(url_for("login")))
+        else:
+            flash('Login unsuccessful')
+            return(redirect(url_for("login")))
+    return render_template("login.html")
 
 @app.route("/register")
 def register():
   # if user already logged in, redirects back to discover
   if 'user' in session:
     return redirect(url_for('root'))
+
   # checking to see if things were submitted
   if (request.args):
     if (bool(request.args["username"]) and bool(request.args["password"])):
@@ -94,6 +116,7 @@ def register():
       inpUser = request.args["username"]
       inpPass = request.args["password"]
       inpConf = request.args["confirmPass"]
+
       if(addUser(inpUser, inpPass, inpConf)):
         flash('Success! Please login.')
         return redirect(url_for("login"))
@@ -111,14 +134,21 @@ def logout():
     return redirect(url_for("root"))
 
 def addUser(user, pswd, conf):
-    if (utl.dbfunctions.userExists(user)):
-        flash('Username already taken. Please try again.')
-        return False
-    if (pswd == conf):
-        utl.dbfunctions.adduser(user,pswd)
-        return True
-    else:
-        flash('Passwords do not match. Please try again.')
+  userList = updateUsers()
+  for row in userList:
+    if user == row[0]:
+      flash('Username already taken. Please try again.')
+      return False
+  if (pswd == conf):
+    # SQLite3 is being weird with threading, so I've created a separate object
+    with sqlite3.connect(DB_FILE) as connection:
+      cur = connection.cursor()
+      q = "INSERT INTO USER VALUES('{}', '{}');".format(user, pswd) # Successfully registers new user
+      cur.execute(q)
+      connection.commit()
+    return True
+  else:
+    flash('Passwords do not match. Please try again.')
     return False
 
 @app.route("/play")
@@ -137,51 +167,12 @@ def profile():
     return render_template("profile.html",
     title = "Profile - {}".format(session["user"]), heading = session["user"],sessionstatus = "user" in session)
 
-@app.route("/blackjack")
-def playBlackJack():
-    req = urllib.request.urlopen(
-        "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6"
-        )
-    res = req.read()
-    deck = json.loads(res)
-    session['deckid'] = deck["deckid"]
-    shuffle()
-    users = utl.dbfunctions.getusercards()
-    ours =  utl.dbfunctions.getourcards()
-    return render_template('blackjack.html',ourcards = ours,usercards = users)
-
-def shuffle():
-    if not ('deckid' in session):
-        return url_for('playBlackJack')
-    req = urllib.request.urlopen(
-        "https://deckofcardsapi.com/api/deck/" + session['deckid'] + "/shuffle/?deck_count=6"
-        )
-    return True
-
-@app.route("/draw")
-def drawCard():
-    if not ('deckid' in session):
-        return url_for('playBlackJack')
-    req = urllib.request.urlopen(
-        "https://deckofcardsapi.com/api/deck/" + session['deckid'] +"/draw/?count=1"
-        )
-    res = req.read()
-    card = json.loads(res)
-    utl.dbfunctions.userdraw(card[image],card[value])
-    return redirect(url_for('playBlackJack'))
-
-
+#https://gist.github.com/straker/ff00b4b49669ad3dec890306d348adc4
 @app.route("/snake")
 def snake():
     if "user" not in session:
         return redirect(url_for('root'))
     return render_template("snake.html", heading = session["user"],sessionstatus = "user" in session)
-
-@app.route("/indexx")
-def indexx():
-    if "user" not in session:
-        return redirect(url_for('root'))
-    return render_template("indexx.html", heading = session["user"],sessionstatus = "user" in session)
 
 if __name__ == "__main__":
     app.debug = True
